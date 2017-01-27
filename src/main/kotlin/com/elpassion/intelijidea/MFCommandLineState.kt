470f19b495a92d781deef42763641cf95a4d07b8
Kasper Kondzielski <kasper.kondzielski@elpassion.pl>
2017-01-27 13:16:55 +0100

    Compile project using project workDir

--- src/main/kotlin/com/elpassion/intelijidea/MFCommandLineState.kt
+++ src/main/kotlin/com/elpassion/intelijidea/MFCommandLineState.kt
@@ -6,9 +6,9 @@ import com.intellij.execution.process.OSProcessHandler
 import com.intellij.execution.process.ProcessHandler
 import com.intellij.execution.runners.ExecutionEnvironment
 
-class MFCommandLineState(executionEnvironment: ExecutionEnvironment) : CommandLineState(executionEnvironment) {
+class MFCommandLineState(private val executionEnvironment: ExecutionEnvironment) : CommandLineState(executionEnvironment) {
     override fun startProcess(): ProcessHandler {
-        return OSProcessHandler(GeneralCommandLine("bash", "mainframer.sh ./gradlew test"))
+        val project = executionEnvironment.project
+        return OSProcessHandler(GeneralCommandLine("./gradlew", "assembleDebug").withWorkDirectory(project.basePath))
     }
 }
-
