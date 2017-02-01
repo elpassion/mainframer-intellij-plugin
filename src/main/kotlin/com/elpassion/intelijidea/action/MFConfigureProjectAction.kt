e26295c2c0de8e245dc9b2208008e98882e2369e
Paweł Gajda <pawegio@gmail.com>
2017-02-01 14:56:08 +0100

    #15 Create configure project action performing nothing

---
+++ src/main/kotlin/com/elpassion/intelijidea/action/MFConfigureProjectAction.kt
@@ -0,0 +1,11 @@
+package com.elpassion.intelijidea.action
+
+import com.intellij.openapi.actionSystem.AnAction
+import com.intellij.openapi.actionSystem.AnActionEvent
+
+class MFConfigureProjectAction : AnAction("Configure Mainframer in Project") {
+
+    override fun actionPerformed(e: AnActionEvent?) {
+        //TODO: configure mainframer
+    }
+}
