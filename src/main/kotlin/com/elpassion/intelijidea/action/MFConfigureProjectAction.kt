b1a14c921b9ecc9ccd5a23bb94fa05c463719d6b
Paweł Gajda <pawegio@gmail.com>
2017-02-01 18:01:33 +0100

    #15 Show confirmation after configuring complete

--- src/main/kotlin/com/elpassion/intelijidea/action/MFConfigureProjectAction.kt
+++ src/main/kotlin/com/elpassion/intelijidea/action/MFConfigureProjectAction.kt
@@ -1,14 +1,13 @@
 package com.elpassion.intelijidea.action
 
 import com.elpassion.intelijidea.common.MFDownloader
-import com.elpassion.intelijidea.util.hasChild
-import com.elpassion.intelijidea.util.mfFilename
-import com.elpassion.intelijidea.util.mfScriptDownloadUrl
+import com.elpassion.intelijidea.util.*
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.project.Project
+import com.intellij.openapi.ui.Messages
 
-class MFConfigureProjectAction : AnAction("Configure Mainframer in Project") {
+class MFConfigureProjectAction : AnAction(MF_CONFIGURE_PROJECT) {
 
     override fun actionPerformed(event: AnActionEvent?) {
         event?.project?.let { configureMainframerInProject(it) }
@@ -18,5 +17,10 @@ class MFConfigureProjectAction : AnAction("Configure Mainframer in Project") {
         if (!project.baseDir.hasChild(mfFilename)) {
             MFDownloader.downloadFileToProject(mfScriptDownloadUrl, project, mfFilename)
         }
+        Messages.showInfoMessage("Mainframer configured in your project!", MF_CONFIGURE_PROJECT)
+    }
+
+    companion object {
+        private val MF_CONFIGURE_PROJECT = "Configure Mainframer in Project"
     }
 }
