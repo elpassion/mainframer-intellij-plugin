3c6fdc049f5cd34d5e5597e43ae75422f031a05b
Dominik <dominik.malantowicz@gmail.com>
2017-02-02 15:03:00 +0100

    [refactoring] Extract method arguments to fields to make code more clear

--- src/main/kotlin/com/elpassion/intelijidea/common/MFDownloader.kt
+++ src/main/kotlin/com/elpassion/intelijidea/common/MFDownloader.kt
@@ -8,11 +8,13 @@ import java.io.File
 object MFDownloader {
 
     fun downloadFileToProject(url: String, project: Project, outputFilename: String) {
-        DownloadUtil.provideDataWithProgressSynchronously(project,
-                "Downloading file", "Downloading ${DownloadUtil.CONTENT_LENGTH_TEMPLATE}...", {
+        val title = "Downloading file"
+        val message = "Downloading ${DownloadUtil.CONTENT_LENGTH_TEMPLATE}..."
+        val action = {
             val progressIndicator = ProgressManager.getInstance().progressIndicator
             DownloadUtil.downloadAtomically(progressIndicator, url, File(project.basePath, outputFilename))
             project.baseDir.refresh(true, false)
-        }, null)
+        }
+        DownloadUtil.provideDataWithProgressSynchronously(project, title, message, action, null)
     }
 }
