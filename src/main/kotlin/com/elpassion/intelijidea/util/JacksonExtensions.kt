1afc7129c59cc7d7e62a17ab09df46441b0e8541
Paweł Gajda <pawegio@gmail.com>
2017-02-03 10:21:58 +0100

    [Refactoring] Reuse object mapper instance

--- src/main/kotlin/com/elpassion/intelijidea/util/JacksonExtensions.kt
+++
@@ -1,8 +0,0 @@
-package com.elpassion.intelijidea.util
-
-import com.fasterxml.jackson.databind.ObjectMapper
-import java.io.Serializable
-
-fun Any.toJson(): String = ObjectMapper().writeValueAsString(this)
-
-inline fun <reified T : Serializable>  String.fromJson(): T = ObjectMapper().readValue(this, T::class.java)
---
+++ src/main/kotlin/com/elpassion/intelijidea/util/jackson.kt
@@ -0,0 +1,10 @@
+package com.elpassion.intelijidea.util
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import java.io.Serializable
+
+val objectMapper by lazy { ObjectMapper() }
+
+fun Any.toJson(): String = objectMapper.writeValueAsString(this)
+
+inline fun <reified T : Serializable> String.fromJson(): T = objectMapper.readValue(this, T::class.java)
