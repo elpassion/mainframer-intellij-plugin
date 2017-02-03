4ace43214ac41f2787f0e3a4364d21874f64ae48
Dominik <dominik.malantowicz@gmail.com>
2017-02-03 11:42:18 +0100

    [refactoring] Kotlinize code

--- src/main/kotlin/com/elpassion/intelijidea/action/MFRestoreDefaultBeforeRunTasksAction.kt
+++ src/main/kotlin/com/elpassion/intelijidea/action/MFRestoreDefaultBeforeRunTasksAction.kt
@@ -7,11 +7,11 @@ import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.ui.Messages
 
 class MFRestoreDefaultBeforeRunTasksAction : AnAction(MF_RESTORE_DEFAULT_BEFORE_RUN_TASK_ACTION) {
+
     override fun actionPerformed(event: AnActionEvent) {
-        val project = event.project
-        val runManager = RunManagerEx.getInstanceEx(project)
-        if (project != null) {
-            restoreDefaultBeforeRunTasks(runManager, project)
+        event.project?.let {
+            val runManager = RunManagerEx.getInstanceEx(it)
+            restoreDefaultBeforeRunTasks(runManager, it)
             Messages.showInfoMessage("BeforeRun tasks default configuration restored.", MF_RESTORE_DEFAULT_BEFORE_RUN_TASK_ACTION)
         }
     }
