bcd9aeadca6743041ac88ffc177cf87d3e525fb7
Marcin Suska <marcin.suska@elpassion.pl>
2017-02-06 17:31:23 +0100

    [#22] Do not show version selection dialog until api call not return data

--- src/main/kotlin/com/elpassion/intelijidea/action/configure/MFConfigureProjectAction.kt
+++ src/main/kotlin/com/elpassion/intelijidea/action/configure/MFConfigureProjectAction.kt
@@ -1,26 +1,45 @@
 package com.elpassion.intelijidea.action.configure
 
 import com.elpassion.intelijidea.action.configure.releases.MFConfigureProjectDialog
+import com.elpassion.intelijidea.action.configure.releases.api.provideGithubApi
+import com.elpassion.intelijidea.action.configure.releases.api.provideGithubRetrofit
+import com.elpassion.intelijidea.action.configure.releases.service.MFVersionsReleaseService
 import com.elpassion.intelijidea.common.MFDownloader
 import com.elpassion.intelijidea.util.getMfToolDownloadUrl
 import com.elpassion.intelijidea.util.mfFilename
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.application.ApplicationManager
+import com.intellij.openapi.progress.ProgressIndicator
+import com.intellij.openapi.progress.ProgressManager
+import com.intellij.openapi.progress.Task
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.ui.Messages
 import com.intellij.platform.templates.github.Outcome
 
 class MFConfigureProjectAction : AnAction(MF_CONFIGURE_PROJECT) {
+    private val service = MFVersionsReleaseService(provideGithubApi(provideGithubRetrofit()))
 
-    override fun actionPerformed(event: AnActionEvent?) {
-        event?.project?.configureMainframer()
+    override fun actionPerformed(event: AnActionEvent) {
+        event.project?.configureMainframer()
     }
 
     private fun Project.configureMainframer() {
-        MFConfigureProjectDialog(this) { version ->
-            val outcome = downloadMainframer(version)
-            Messages.showInfoMessage(outcome.getMessage(), MF_CONFIGURE_PROJECT)
-        }.show()
+        ProgressManager.getInstance().run(object : Task.Backgroundable(this, "Downloading mainframer versions") {
+            override fun run(indicator: ProgressIndicator) {
+                val releaseVersionsList = service.getVersions()
+                showMFConfigureDialog(releaseVersionsList)
+            }
+        })
+    }
+
+    private fun Project.showMFConfigureDialog(versionsList: List<String>) {
+        ApplicationManager.getApplication().invokeLater {
+            MFConfigureProjectDialog(this, versionsList) { version ->
+                val outcome = downloadMainframer(version)
+                Messages.showInfoMessage(outcome.getMessage(), MF_CONFIGURE_PROJECT)
+            }.show()
+        }
     }
 
     private fun Outcome<Unit>.getMessage() = when {
--- src/main/kotlin/com/elpassion/intelijidea/action/configure/releases/MFConfigureProjectDialog.kt
+++ src/main/kotlin/com/elpassion/intelijidea/action/configure/releases/MFConfigureProjectDialog.kt
@@ -1,21 +1,14 @@
 package com.elpassion.intelijidea.action.configure.releases
 
-import com.elpassion.intelijidea.action.configure.releases.api.provideGithubApi
-import com.elpassion.intelijidea.action.configure.releases.api.provideGithubRetrofit
-import com.elpassion.intelijidea.action.configure.releases.service.MFVersionsReleaseService
 import com.elpassion.intelijidea.action.configure.releases.ui.MFConfigureProjectForm
-import com.intellij.openapi.progress.ProgressIndicator
-import com.intellij.openapi.progress.ProgressManager
-import com.intellij.openapi.progress.Task
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.ui.DialogWrapper
 import javax.swing.JComponent
 
 
-class MFConfigureProjectDialog(private val project: Project, private val onVersion: (String) -> Unit) : DialogWrapper(project, false) {
+class MFConfigureProjectDialog(project: Project, val releaseVersionsList: List<String>, private val onVersion: (String) -> Unit) : DialogWrapper(project, false) {
 
     private val form = MFConfigureProjectForm()
-    private val service = MFVersionsReleaseService(provideGithubApi(provideGithubRetrofit()))
 
     init {
         title = "Configure Mainframer in Project"
@@ -23,12 +16,7 @@ class MFConfigureProjectDialog(private val project: Project, private val onVersi
     }
 
     override fun createCenterPanel(): JComponent {
-        ProgressManager.getInstance().run(object : Task.Backgroundable(project, "Downloading mainframer version") {
-            override fun run(indicator: ProgressIndicator) {
-                val releaseVersionsList = service.getVersions()
-                form.versionComboBox.model = MFComboBoxViewModel(releaseVersionsList)
-            }
-        })
+        form.versionComboBox.model = MFComboBoxViewModel(releaseVersionsList)
         return form.panel
     }
 
