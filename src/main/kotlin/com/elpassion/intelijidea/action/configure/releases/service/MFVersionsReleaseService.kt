32b30dd4c6a89666748ef1361127c1c983a660f5
Marcin Suska <marcin.suska@elpassion.pl>
2017-02-06 14:32:49 +0100

    [#22] Fix spelling error for MF release version

--- src/main/kotlin/com/elpassion/intelijidea/action/configure/releases/service/MFVersionsReleaseService.kt
+++ src/main/kotlin/com/elpassion/intelijidea/action/configure/releases/service/MFVersionsReleaseService.kt
@@ -3,6 +3,6 @@ package com.elpassion.intelijidea.action.configure.releases.service
 
 class MFVersionsReleaseService {
     fun getVersions(): List<String> {
-        return listOf("2.00", "1.1.2", "1.1.1", "1.1.0", "1.0.2", "1.0.1", "1.0.0")
+        return listOf("2.0.0", "1.1.2", "1.1.1", "1.1.0", "1.0.2", "1.0.1", "1.0.0")
     }
 }
