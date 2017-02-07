f1b415244101848df787d937c464a1683dcc68eb
Karol Kowalski <ordon1313@gmail.com>
2017-02-07 13:46:10 +0100

    [#31] Extract MFConfigureProjectActionController

--- src/main/kotlin/com/elpassion/intelijidea/action/configure/MFConfigureProjectAction.kt
+++ src/main/kotlin/com/elpassion/intelijidea/action/configure/MFConfigureProjectAction.kt
@@ -9,45 +9,60 @@ import com.elpassion.intelijidea.util.getMfToolDownloadUrl
 import com.elpassion.intelijidea.util.mfFilename
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
-import com.intellij.openapi.project.Project
 import com.intellij.openapi.ui.Messages
 import com.intellij.platform.templates.github.Outcome
+import io.reactivex.Observable
+import io.reactivex.Scheduler
 
 class MFConfigureProjectAction : AnAction(MF_CONFIGURE_PROJECT) {
-    private val service = MFVersionsReleaseService(provideGithubApi(provideGithubRetrofit()))
-
     override fun actionPerformed(event: AnActionEvent) {
-        event.project?.configureMainframer()
+        event.project?.let {
+            MFConfigureProjectActionController(
+                    service = MFVersionsReleaseService(provideGithubApi(provideGithubRetrofit())),
+                    versionChooser = { versionsList ->
+                        Observable.create({ emitter ->
+                            MFConfigureProjectDialog(it, versionsList, { version ->
+                                emitter.onNext(version)
+                                emitter.onComplete()
+                            }).show()
+                        })
+                    },
+                    downloadMainframer = { version ->
+                        Observable.just(
+                                MFDownloader.downloadFileToProject(getMfToolDownloadUrl(version), it, mfFilename)
+                        )
+                    },
+                    progressScheduler = ProgressScheduler(it, "Downloading mainframer versions")).configureMainframer()
+        }
     }
-
-    private fun Project.configureMainframer() {
-        service.getVersions()
-                .subscribeOn(ProgressScheduler(this, "Downloading mainframer versions"))
-                .observeOn(UIScheduler)
-                .subscribe({
-                    showMFConfigureDialog(it)
-                }, {
-                    Messages.showInfoMessage(it.message, MF_CONFIGURE_PROJECT)
-                })
-    }
-
-    private fun Project.showMFConfigureDialog(versionsList: List<String>) {
-        MFConfigureProjectDialog(this, versionsList) { version ->
-            val outcome = downloadMainframer(version)
-            Messages.showInfoMessage(outcome.getMessage(), MF_CONFIGURE_PROJECT)
-        }.show()
-    }
-
-    private fun Outcome<Unit>.getMessage() = when {
-        isCancelled -> "Mainframer configuration canceled"
-        exception != null -> "Error during mainframer configuration"
-        else -> "Mainframer configured in your project!"
-    }
-
-    private fun Project.downloadMainframer(version: String) =
-            MFDownloader.downloadFileToProject(getMfToolDownloadUrl(version), this, mfFilename)
 
     companion object {
         private val MF_CONFIGURE_PROJECT = "Configure Mainframer in Project"
+    }
+
+    class MFConfigureProjectActionController(
+            val service: MFVersionsReleaseService,
+            val versionChooser: (List<String>) -> Observable<String>,
+            val downloadMainframer: (String) -> Observable<Outcome<Unit>>,
+            val progressScheduler: Scheduler) {
+
+        fun configureMainframer() {
+            service.getVersions()
+                    .subscribeOn(progressScheduler)
+                    .observeOn(UIScheduler)
+                    .flatMap(versionChooser)
+                    .flatMap(downloadMainframer)
+                    .subscribe({
+                        Messages.showInfoMessage(it.getMessage(), MFConfigureProjectAction.MF_CONFIGURE_PROJECT)
+                    }, {
+                        Messages.showInfoMessage(it.message, MFConfigureProjectAction.MF_CONFIGURE_PROJECT)
+                    })
+        }
+
+        private fun Outcome<Unit>.getMessage() = when {
+            isCancelled -> "Mainframer configuration canceled"
+            exception != null -> "Error during mainframer configuration"
+            else -> "Mainframer configured in your project!"
+        }
     }
 }
