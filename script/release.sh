b720aff827fbe092a5403d6d612e2dd2b13e8f7e
Kasper Kondzielski <kasper.kondzielski@elpassion.pl>
2017-02-07 18:01:33 +0100

    #28 Change approach of releases: run them locally

--- script/release.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-git config --global user.email "travis@ci.com"
-git config --global user.name "Travis CI"
-git checkout -b release_from_travis
-./gradlew release -Prelease.useAutomaticVersion=true
-git checkout master
-git merge --no-ff release_from_travis
-git push
-git push origin --delete release_from_travis
