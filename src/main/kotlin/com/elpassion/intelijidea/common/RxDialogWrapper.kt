5ae4264b079e214d495ee4c70ca6734f56bde11f
Karol Kowalski <ordon1313@gmail.com>
2017-02-08 16:59:59 +0100

    Simplify RxDialogWrapper utilizing PublishSubject

--- src/main/kotlin/com/elpassion/intelijidea/common/RxDialogWrapper.kt
+++ src/main/kotlin/com/elpassion/intelijidea/common/RxDialogWrapper.kt
@@ -2,36 +2,29 @@ package com.elpassion.intelijidea.common
 
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.ui.DialogWrapper
-import io.reactivex.Emitter
 import io.reactivex.Observable
 import io.reactivex.functions.Cancellable
+import io.reactivex.subjects.PublishSubject
 
 abstract class RxDialogWrapper<T>(project: Project) : DialogWrapper(project, false), Cancellable {
 
-    private var emitter: Emitter<Result<T>>? = null
+    private val subject = PublishSubject.create<Result<T>>()
 
     abstract fun getSuccessResult(): T
 
     override final fun doOKAction() {
         super.doOKAction()
-        emitter?.let {
-            it.onNext(Result.Success(getSuccessResult()))
-            it.onComplete()
-            emitter = null
-        }
+        subject.onNext(Result.Success(getSuccessResult()))
+        subject.onComplete()
     }
 
     override final fun doCancelAction() {
         super.doCancelAction()
-        emitter?.let {
-            it.onNext(Result.Canceled())
-            it.onComplete()
-            emitter = null
-        }
+        subject.onNext(Result.Canceled())
+        subject.onComplete()
     }
 
     override final fun cancel() {
-        emitter = null
         close(CANCEL_EXIT_CODE)
     }
 
@@ -39,11 +32,5 @@ abstract class RxDialogWrapper<T>(project: Project) : DialogWrapper(project, fal
         throw IllegalAccessError("Use showAsObservable()")
     }
 
-    fun showAsObservable(): Observable<Result<T>> {
-        return Observable.create<Result<T>> { emitter ->
-            this.emitter = emitter
-            emitter.setCancellable(this)
-            super.show()
-        }
-    }
+    fun showAsObservable(): Observable<Result<T>> = subject.doOnSubscribe { super.show() }
 }
