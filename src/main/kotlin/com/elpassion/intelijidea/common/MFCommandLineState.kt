6bf47a3aea48b166a14ea72d7bb76c0f5cfbab7d
Kasper Kondzielski <kasper.kondzielski@elpassion.pl>
2017-02-08 16:58:22 +0100

    #44 Display out from execution process

--- src/main/kotlin/com/elpassion/intelijidea/common/MFCommandLineState.kt
+++ src/main/kotlin/com/elpassion/intelijidea/common/MFCommandLineState.kt
@@ -1,9 +1,13 @@
 package com.elpassion.intelijidea.common
 
+import com.intellij.execution.ExecutionManager
+import com.intellij.execution.ExecutionResult
+import com.intellij.execution.Executor
 import com.intellij.execution.configurations.CommandLineState
-import com.intellij.execution.process.ColoredProcessHandler
-import com.intellij.execution.process.ProcessHandler
+import com.intellij.execution.process.*
 import com.intellij.execution.runners.ExecutionEnvironment
+import com.intellij.execution.runners.ProgramRunner
+import com.intellij.openapi.util.Key
 
 class MFCommandLineState(private val executionEnvironment: ExecutionEnvironment,
                          private val mainframerPath: String?,
@@ -14,4 +18,28 @@ class MFCommandLineState(private val executionEnvironment: ExecutionEnvironment,
             ColoredProcessHandler(createCommandLine().withWorkDirectory(executionEnvironment.project.basePath))
 
     private fun createCommandLine() = MFCommandLine(mainframerPath, buildCommand, taskName)
+
+    override fun execute(executor: Executor, runner: ProgramRunner<*>): ExecutionResult {
+        return super.execute(executor, runner).apply {
+            with(processHandler) {
+                addOnTextAvailableListener(onText = { bringConsoleToFront(executor) })
+            }
+        }
+    }
+
+    private fun ProcessHandler.bringConsoleToFront(executor: Executor) {
+        ExecutionManager.getInstance(executionEnvironment.project).contentManager.toFrontRunContent(executor, this)
+    }
+}
+
+private fun ProcessHandler.addOnTextAvailableListener(onText: () -> Unit) {
+    addProcessListener(createOnTextAvailableListener(onText))
+}
+
+private fun createOnTextAvailableListener(onText: () -> Unit) = object : ProcessAdapter() {
+    override fun onTextAvailable(event: ProcessEvent, outputType: Key<*>) {
+        if (outputType === ProcessOutputTypes.STDOUT || outputType === ProcessOutputTypes.STDERR) {
+            onText()
+        }
+    }
 }
