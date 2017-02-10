b7ea561d1bfdcfbb0360e422d2802ed318932126
Paweł Gajda <pawegio@gmail.com>
2017-02-10 16:24:48 +0100

    #42 Throw execution exception when data without tool file

--- src/test/kotlin/com/elpassion/intelijidea/integration/MFRunConfigurationExecutionTest.kt
+++ src/test/kotlin/com/elpassion/intelijidea/integration/MFRunConfigurationExecutionTest.kt
@@ -13,7 +13,7 @@ import com.intellij.testFramework.fixtures.LightPlatformCodeInsightFixtureTestCa
 
 class MFRunConfigurationExecutionTest : LightPlatformCodeInsightFixtureTestCase() {
 
-    fun testShouldThrowExecutionExceptionWhenToolNotFound() {
+    fun testShouldThrowExecutionExceptionWhenConfigurationDataIsNull() {
         val exception = assertThrows(ExecutionException::class.java) {
             val config = createRunConfiguration()
             val executor = DefaultRunExecutor.getRunExecutorInstance()
@@ -22,8 +22,18 @@ class MFRunConfigurationExecutionTest : LightPlatformCodeInsightFixtureTestCase(
         assertEquals("Mainframer tool cannot be found", exception.message)
     }
 
+    fun testShouldThrowExecutionExceptionWhenToolNotFound() {
+        val exception = assertThrows(ExecutionException::class.java) {
+            val data = createConfigurationData(withToolFile = false)
+            val config = createRunConfiguration(data)
+            val executor = DefaultRunExecutor.getRunExecutorInstance()
+            ExecutionEnvironmentBuilder.create(project, executor, config).buildAndExecute()
+        }
+        assertEquals("Mainframer tool cannot be found", exception.message)
+    }
+
     fun testShouldStartExecutionWithoutExceptionOnToolFoundInDefinedPath() {
-        val data = createConfigurationData()
+        val data = createConfigurationData(withToolFile = true)
         val config = createRunConfiguration(data)
         val executor = DefaultRunExecutor.getRunExecutorInstance()
         ExecutionEnvironmentBuilder.create(project, executor, config).buildAndExecute()
@@ -34,9 +44,11 @@ class MFRunConfigurationExecutionTest : LightPlatformCodeInsightFixtureTestCase(
                 data = configurationData
             }
 
-    private fun createConfigurationData(): MFRunnerConfigurationData {
+    private fun createConfigurationData(withToolFile: Boolean): MFRunnerConfigurationData {
         val mfDir = FileUtil.createTempDirectory("", "mf")
-        FileUtil.createTempFile(mfDir, "mainframer", ".sh")
+        if (withToolFile) {
+            FileUtil.createTempFile(mfDir, "mainframer", ".sh")
+        }
         val data = MFRunnerConfigurationData(mainframerPath = mfDir.absolutePath)
         return data
     }
