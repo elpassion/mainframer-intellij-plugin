d85b3004735958e13e98f98bb6d3a54d1377b48b
Paweł Gajda <pawegio@gmail.com>
2017-02-10 16:10:26 +0100

    #42 Start execution without exception on tool found in defined path

--- src/test/kotlin/com/elpassion/intelijidea/integration/MFRunConfigurationExecutionTest.kt
+++ src/test/kotlin/com/elpassion/intelijidea/integration/MFRunConfigurationExecutionTest.kt
@@ -3,20 +3,33 @@ package com.elpassion.intelijidea.integration
 import com.elpassion.intelijidea.configuration.MFConfigurationFactory
 import com.elpassion.intelijidea.configuration.MFRunConfigurationType
 import com.elpassion.intelijidea.configuration.MFRunnerConfiguration
+import com.elpassion.intelijidea.configuration.MFRunnerConfigurationData
 import com.elpassion.intelijidea.configuration.common.assertThrows
 import com.intellij.execution.ExecutionException
 import com.intellij.execution.executors.DefaultRunExecutor
 import com.intellij.execution.runners.ExecutionEnvironmentBuilder
+import com.intellij.openapi.util.io.FileUtil
 import com.intellij.testFramework.fixtures.LightPlatformCodeInsightFixtureTestCase
 
 class MFRunConfigurationExecutionTest : LightPlatformCodeInsightFixtureTestCase() {
 
-    fun testShouldThrowExecutionExceptionWhenToolNotFoundOnExecution() {
+    fun testShouldThrowExecutionExceptionWhenToolNotFound() {
         val exception = assertThrows(ExecutionException::class.java) {
             val config = MFRunnerConfiguration(project, MFConfigurationFactory(MFRunConfigurationType()), "", {})
             val executor = DefaultRunExecutor.getRunExecutorInstance()
             ExecutionEnvironmentBuilder.create(project, executor, config).buildAndExecute()
         }
         assertEquals("Mainframer tool cannot be found", exception.message)
+    }
+
+    fun testShouldStartExecutionWithoutExceptionOnToolFoundInDefinedPath() {
+        val mfDir = FileUtil.createTempDirectory("", "mf")
+        FileUtil.createTempFile(mfDir, "mainframer", ".sh")
+        val configurationData = MFRunnerConfigurationData(mainframerPath = mfDir.absolutePath)
+        val config = MFRunnerConfiguration(project, MFConfigurationFactory(MFRunConfigurationType()), "", {}).apply {
+            data = configurationData
+        }
+        val executor = DefaultRunExecutor.getRunExecutorInstance()
+        ExecutionEnvironmentBuilder.create(project, executor, config).buildAndExecute()
     }
 }
