3e9de5c98f329960c000c0677f848b2a7c1f6996
Marcin Suska <marcin.suska@elpassion.pl>
2017-02-21 16:46:03 +0100

    #69 Add unified validator to MFSettingsEditor

--- src/main/kotlin/com/elpassion/intelijidea/configuration/MFSettingsEditor.kt
+++ src/main/kotlin/com/elpassion/intelijidea/configuration/MFSettingsEditor.kt
@@ -1,10 +1,13 @@
 package com.elpassion.intelijidea.configuration
 
+import com.elpassion.intelijidea.common.BuildCommandValidator
+import com.elpassion.intelijidea.common.MainframerPathValidator
+import com.elpassion.intelijidea.common.TaskFieldValidator
+import com.elpassion.intelijidea.common.validateForm
 import com.elpassion.intelijidea.configuration.ui.MFSettingsEditorPanel
 import com.intellij.openapi.options.ConfigurationException
 import com.intellij.openapi.options.SettingsEditor
 import com.intellij.openapi.project.Project
-import java.io.File
 import javax.swing.JComponent
 
 
@@ -17,28 +20,25 @@ class MFSettingsEditor(project: Project) : SettingsEditor<MFRunConfiguration>() 
     }
 
     override fun applyEditorTo(configuration: MFRunConfiguration) {
-        configuration.data = configuration.data?.copy(
-                buildCommand = mainEditorPanel.buildCommand.text.trim(),
-                taskName = mainEditorPanel.taskName.text.trim(),
-                mainframerPath = mainEditorPanel.mainframerTool.text.trim())
-        configuration.data?.run {
-            if (mainframerPath.isNullOrEmpty()) {
-                throw ConfigurationException("Mainframer path cannot be empty")
-            }
-            if (File(mainframerPath).exists().not()) {
-                throw ConfigurationException("Mainframer path is invalid")
-            }
-            if (taskName.isNullOrEmpty()) {
-                throw ConfigurationException("Task cannot be empty")
-            }
-        }
+        configuration.data = updatedConfigurationData(configuration)
+        doValidate()
+    }
+
+    private fun updatedConfigurationData(configuration: MFRunConfiguration) =
+            configuration.data?.copy(
+                    buildCommand = mainEditorPanel.buildCommand.text.trim(),
+                    taskName = mainEditorPanel.taskName.text.trim(),
+                    mainframerPath = mainEditorPanel.mainframerTool.text.trim())
+
+    private fun doValidate() = with(mainEditorPanel) {
+        validateForm(BuildCommandValidator(buildCommand), TaskFieldValidator(taskName), MainframerPathValidator(mainframerTool))?.let { throw ConfigurationException(it.message) }
     }
 
     override fun resetEditorFrom(configuration: MFRunConfiguration) {
-        configuration.data.let {
-            mainEditorPanel.buildCommand.text = it?.buildCommand
-            mainEditorPanel.taskName.text = it?.taskName
-            mainEditorPanel.mainframerTool.text = it?.mainframerPath ?: ""
+        configuration.data?.let {
+            mainEditorPanel.buildCommand.text = it.buildCommand
+            mainEditorPanel.taskName.text = it.taskName
+            mainEditorPanel.mainframerTool.text = it.mainframerPath ?: ""
         }
     }
 }
