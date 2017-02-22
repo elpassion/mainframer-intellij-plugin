6c2c3dc30e2027273f73ccebb17816692af4219d
ghostbuster <kghost0@gmail.com>
2017-02-22 21:22:48 +0100

    [#81] Drop support for 1.x versions

--- src/main/kotlin/com/elpassion/intelijidea/action/configure/releases/service/MFReleasesFetcher.kt
+++ src/main/kotlin/com/elpassion/intelijidea/action/configure/releases/service/MFReleasesFetcher.kt
@@ -1,8 +1,15 @@
 package com.elpassion.intelijidea.action.configure.releases.service
 
 import com.elpassion.intelijidea.action.configure.releases.api.GitHubApi
+import com.elpassion.intelijidea.action.configure.releases.api.ReleaseApiModel
+import io.reactivex.Single
 
 fun mfReleasesFetcher(api: GitHubApi) = {
     api.listReleases()
-            .map { it.map { it.tagName.drop(1) } }
+            .mapWithoutVPrefix()
+            .drop1xVersions()
 }
+
+private fun Single<List<String>>.drop1xVersions() = map { it.filter { !it.matches("1.\\d.\\d".toRegex()) } }
+
+private fun Single<List<ReleaseApiModel>>.mapWithoutVPrefix() = map { it.map { it.tagName.drop(1) } }
--- src/test/kotlin/com/elpassion/intelijidea/action/configure/releases/service/MFReleasesFetcherTest.kt
+++ src/test/kotlin/com/elpassion/intelijidea/action/configure/releases/service/MFReleasesFetcherTest.kt
@@ -3,11 +3,8 @@ package com.elpassion.intelijidea.action.configure.releases.service
 import com.elpassion.android.commons.rxjavatest.thenJust
 import com.elpassion.intelijidea.action.configure.releases.api.GitHubApi
 import com.elpassion.intelijidea.action.configure.releases.api.ReleaseApiModel
-import com.nhaarman.mockito_kotlin.doReturn
 import com.nhaarman.mockito_kotlin.mock
 import com.nhaarman.mockito_kotlin.whenever
-import io.reactivex.Observable
-import io.reactivex.Single
 import org.junit.Test
 
 class MFReleasesFetcherTest {
@@ -16,21 +13,31 @@ class MFReleasesFetcherTest {
 
     @Test
     fun shouldDropFirstCharacter() {
-        stubApiToReturn(ReleaseApiModel("v1.1.2"))
+        stubApiToReturn(ReleaseApiModel("v2.1.2"))
 
         releasesFetcher()
                 .test()
-                .assertValue{ it.first() == "1.1.2"}
+                .assertValue{ it.first() == "2.1.2"}
     }
 
     @Test
     fun shouldProperMapReleaseApiModelToString() {
-        stubApiToReturn(ReleaseApiModel("v1.1.2"), ReleaseApiModel("v1.1.3"))
+        stubApiToReturn(ReleaseApiModel("v2.1.2"), ReleaseApiModel("v2.1.3"))
 
         releasesFetcher()
                 .test()
-                .assertValue{ it[0] == "1.1.2"}
-                .assertValue{ it[1] == "1.1.3"}
+                .assertValue{ it[0] == "2.1.2"}
+                .assertValue{ it[1] == "2.1.3"}
+    }
+
+    @Test
+    fun shouldNotReturnAnyVariantsOfVersionOne() {
+        stubApiToReturn(ReleaseApiModel("v1.1.2"), ReleaseApiModel("v2.0.0"))
+
+        releasesFetcher()
+                .test()
+                .assertValue { it[0] == "2.0.0" }
+                .assertValueCount(1)
     }
 
     private fun stubApiToReturn(vararg response: ReleaseApiModel) =
