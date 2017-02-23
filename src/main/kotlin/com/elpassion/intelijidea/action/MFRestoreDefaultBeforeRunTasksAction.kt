5435154558220b1d7c8f9eb721cf77994d29c5bc
Paweł Gajda <pawegio@gmail.com>
2017-02-23 10:36:40 +0100

    #72 Apply CR: Replace dialog with balloon in restore tasks action

--- src/main/kotlin/com/elpassion/intelijidea/action/MFRestoreDefaultBeforeRunTasksAction.kt
+++ src/main/kotlin/com/elpassion/intelijidea/action/MFRestoreDefaultBeforeRunTasksAction.kt
@@ -1,10 +1,10 @@
 package com.elpassion.intelijidea.action
 
 import com.elpassion.intelijidea.restoreDefaultBeforeRunTasks
+import com.elpassion.intelijidea.util.showInfo
 import com.intellij.execution.RunManagerEx
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
-import com.intellij.openapi.ui.Messages
 
 class MFRestoreDefaultBeforeRunTasksAction : AnAction(MF_RESTORE_DEFAULT_BEFORE_RUN_TASK_ACTION) {
 
@@ -12,7 +12,7 @@ class MFRestoreDefaultBeforeRunTasksAction : AnAction(MF_RESTORE_DEFAULT_BEFORE_
         event.project?.let {
             val runManager = RunManagerEx.getInstanceEx(it)
             restoreDefaultBeforeRunTasks(runManager, it)
-            Messages.showInfoMessage("BeforeRun tasks default configuration restored.", MF_RESTORE_DEFAULT_BEFORE_RUN_TASK_ACTION)
+            showInfo(it, "Restored default configuration of before run tasks.")
         }
     }
 
