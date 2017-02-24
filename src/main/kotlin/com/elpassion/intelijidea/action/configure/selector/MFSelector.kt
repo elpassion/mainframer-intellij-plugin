8da6f9d83f9c452a4a97eb3bc71ca6c888ac084e
Paweł Gajda <pawegio@gmail.com>
2017-02-24 14:20:17 +0100

    #84 Select configurations with mainframer task

--- src/main/kotlin/com/elpassion/intelijidea/action/configure/selector/MFSelector.kt
+++ src/main/kotlin/com/elpassion/intelijidea/action/configure/selector/MFSelector.kt
@@ -1,14 +1,19 @@
 package com.elpassion.intelijidea.action.configure.selector
 
 import com.elpassion.intelijidea.getConfigurations
+import com.elpassion.intelijidea.task.MFBeforeRunTask
 import com.intellij.execution.RunManagerEx
+import com.intellij.execution.configurations.RunConfiguration
 import com.intellij.openapi.project.Project
 import io.reactivex.Observable
 import io.reactivex.Single
 
 fun mfSelector(project: Project, selectorFromUi: (List<MFSelectorItem>) -> Observable<MFSelectorItem>): Single<List<MFSelectorItem>> {
-    val runManager = RunManagerEx.getInstance(project)
+    val runManager = RunManagerEx.getInstanceEx(project)
     val selectorItems = runManager.getConfigurations()
-            .map { MFSelectorItem(it, false) }
+            .map { MFSelectorItem(it, runManager.hasMainframerTask(it)) }
     return selectorFromUi(selectorItems).toList()
 }
+
+private fun RunManagerEx.hasMainframerTask(configuration: RunConfiguration) =
+        getBeforeRunTasks(configuration).any { it is MFBeforeRunTask }
