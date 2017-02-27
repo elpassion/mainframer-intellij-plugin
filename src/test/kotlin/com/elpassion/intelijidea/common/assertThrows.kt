1abeb5a44b96fd597ba0deff6b601aee08403306
Dominik <dominik.malantowicz@gmail.com>
2017-02-27 15:02:07 +0100

    [refactoring] Simplify assertThrows method

--- src/test/kotlin/com/elpassion/intelijidea/common/assertThrows.kt
+++ src/test/kotlin/com/elpassion/intelijidea/common/assertThrows.kt
@@ -1,10 +1,10 @@
 package com.elpassion.intelijidea.common
 
-fun <T : Exception> assertThrows(exceptionType: Class<T>, function: () -> Unit): T {
+fun assertThrows(exceptionType: Class<*>, function: () -> Unit): Exception {
     try {
         function.invoke()
         throw AssertionError("The specified function didn't throw an exception. ")
-    } catch (ex: T) {
+    } catch (ex: Exception) {
         when {
             exceptionType.isAssignableFrom(ex.javaClass) -> return ex
             else -> throw AssertionError("Expected ${exceptionType.name} not thrown. Catched ${ex.javaClass.name}")
