cdeaa6e54ad1a0f7baa049ffd0b568e48f311d25
Kasper Kondzielski <kasper.kondzielski@elpassion.pl>
2017-03-09 14:16:09 +0100

    [Refactoring] Inline class

--- src/main/kotlin/com/elpassion/intelijidea/common/console/RemoteToLocalFileTranslator.kt
+++
@@ -1,7 +0,0 @@
-package com.elpassion.intelijidea.common.console
-
-import com.intellij.openapi.project.Project
-
-object RemoteToLocalFileTranslator {
-    fun translate(project: Project, remoteFilePath: String): String = remoteFilePath.replace("\\D+mainframer/${project.name}".toRegex()) { project.basePath!! }
-}
