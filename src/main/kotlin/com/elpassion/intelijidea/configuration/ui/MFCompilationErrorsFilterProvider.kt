0edfc2826e4c23bda3cb1fac4f3ef848a3acf238
Kasper Kondzielski <kasper.kondzielski@elpassion.pl>
2017-03-09 11:42:18 +0100

    [Refactoring] Convert method to expression

--- src/main/kotlin/com/elpassion/intelijidea/configuration/ui/MFCompilationErrorsFilterProvider.kt
+++ src/main/kotlin/com/elpassion/intelijidea/configuration/ui/MFCompilationErrorsFilterProvider.kt
@@ -2,11 +2,8 @@ package com.elpassion.intelijidea.configuration.ui
 
 import com.elpassion.intelijidea.configuration.RemoteToLocalTranslatingFilter
 import com.intellij.execution.filters.ConsoleFilterProvider
-import com.intellij.execution.filters.Filter
 import com.intellij.openapi.project.Project
 
 class MFCompilationErrorsFilterProvider : ConsoleFilterProvider {
-    override fun getDefaultFilters(project: Project): Array<Filter> {
-        return arrayOf(RemoteToLocalTranslatingFilter(project))
-    }
+    override fun getDefaultFilters(project: Project) = arrayOf(RemoteToLocalTranslatingFilter(project))
 }
