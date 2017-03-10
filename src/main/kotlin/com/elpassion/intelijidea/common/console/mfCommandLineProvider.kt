57f6254f868b69e10e7cd106077432ca256e2055
Kasper Kondzielski <kasper.kondzielski@elpassion.pl>
2017-03-10 15:06:09 +0100

    Merge taskData and runConfiguration

--- src/main/kotlin/com/elpassion/intelijidea/common/console/mfCommandLineProvider.kt
+++ src/main/kotlin/com/elpassion/intelijidea/common/console/mfCommandLineProvider.kt
@@ -1,22 +1,11 @@
 package com.elpassion.intelijidea.common.console
 
 import com.elpassion.intelijidea.MFStateProvider
-import com.elpassion.intelijidea.configuration.MFRunConfigurationData
 import com.elpassion.intelijidea.task.MFTaskData
 import com.intellij.execution.configurations.GeneralCommandLine
 import com.intellij.openapi.project.Project
 
 val mfCommandLineProvider: (Project, MFTaskData) -> GeneralCommandLine = { project, taskData ->
-    with(taskData) {
-        if (MFStateProvider.getInstance(project).isTurnOn) {
-            GeneralCommandLine("bash", mainframerPath, buildCommand, taskName)
-        } else {
-            GeneralCommandLine("bash", buildCommand, taskName)
-        }
-    }.withWorkDirectory(project.basePath)
-}
-
-val mfCommandLineProvider2: (Project, MFRunConfigurationData) -> GeneralCommandLine = { project, taskData ->
     with(taskData) {
         if (MFStateProvider.getInstance(project).isTurnOn) {
             GeneralCommandLine("bash", mainframerPath, buildCommand, taskName)
