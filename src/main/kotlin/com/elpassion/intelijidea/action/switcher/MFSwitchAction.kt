c752f4f1e092c0b1de00064ebdc6ba5e2effd2ec
Kasper Kondzielski <kasper.kondzielski@elpassion.pl>
2017-03-10 17:09:11 +0100

    Add action to easily turn on/off mainframer

---
+++ src/main/kotlin/com/elpassion/intelijidea/action/switcher/MFSwitchAction.kt
@@ -0,0 +1,27 @@
+package com.elpassion.intelijidea.action.switcher
+
+import com.elpassion.intelijidea.MFStateProvider
+import com.elpassion.intelijidea.util.MFIcons
+import com.intellij.icons.AllIcons
+import com.intellij.openapi.actionSystem.AnAction
+import com.intellij.openapi.actionSystem.AnActionEvent
+
+class MFSwitchAction : AnAction(MF_SWTICH_ACTION) {
+    override fun actionPerformed(event: AnActionEvent) {
+        event.project?.let {
+            MFStateProvider.getInstance(it).apply {
+                isTurnOn = !isTurnOn
+            }
+        }
+    }
+
+    override fun update(e: AnActionEvent) {
+        e.project?.let {
+            e.presentation.icon = if(MFStateProvider.getInstance(it).isTurnOn) MFIcons.mainframerIcon else AllIcons.Windows.CloseActive
+        }
+    }
+
+    companion object {
+        private val MF_SWTICH_ACTION = "Quickly turn on/off mainframer"
+    }
+}
