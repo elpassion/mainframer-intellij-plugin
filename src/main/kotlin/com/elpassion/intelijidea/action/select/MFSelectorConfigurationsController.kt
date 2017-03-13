7a2e33d30d3f56054e16a3b25b7f29c9a83dafb6
Marcin Suska <marcin.suska@elpassion.pl>
2017-03-13 13:39:05 +0100

    #108 CR: Replace flatMap by Map in MFSelectorController

--- src/main/kotlin/com/elpassion/intelijidea/action/select/MFSelectorConfigurationsController.kt
+++ src/main/kotlin/com/elpassion/intelijidea/action/select/MFSelectorConfigurationsController.kt
@@ -4,16 +4,16 @@ import com.elpassion.intelijidea.action.configure.selector.MFSelectorResult
 import io.reactivex.Maybe
 
 class MFSelectorConfigurationsController(val manipulateTasks: (MFSelectorResult) -> Unit,
-                                         val selectorResult:()-> Maybe<MFSelectorResult>,
+                                         val selectorResult: () -> Maybe<MFSelectorResult>,
                                          val isSettingsTaskValid: () -> Boolean,
                                          val showMessage: (Int, Int) -> Unit,
                                          val showError: (String) -> Unit) {
     fun configure() {
-        selectorResult().flatMap {
+        selectorResult().map {
             if (!it.toInject.isEmpty() && !isSettingsTaskValid()) {
-                Maybe.error(RuntimeException("Invalid inject configuration"))
+                throw RuntimeException("Invalid inject configuration")
             } else {
-                Maybe.just(it)
+                it
             }
         }.doOnSuccess {
             manipulateTasks(it)
