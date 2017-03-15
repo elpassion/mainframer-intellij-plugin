ad281df5a5d4bcfd6ebacfe49ab7b23483b616e2
Karol Kowalski <ordon1313@gmail.com>
2017-03-15 14:20:58 +0100

    #109 push if statment out of main logic

--- src/main/kotlin/com/elpassion/intelijidea/action/configure/selector/MFSelector.kt
+++ src/main/kotlin/com/elpassion/intelijidea/action/configure/selector/MFSelector.kt
@@ -16,15 +16,9 @@ fun mfSelector(project: Project, uiSelector: MFUiSelector) = {
 }
 
 fun getSelectorResult(uiIn: List<MFSelectorItem>, uiOut: List<MFSelectorItem>, replaceAll: Boolean): MFSelectorResult {
-    return if (replaceAll) {
-        val toInject = getItemsToInject(emptyList(), uiOut)
-        val toRestore = getItemsToRestore(uiIn, uiOut)
-        MFSelectorResult(toInject, toRestore)
-    } else {
-        val toInject = getItemsToInject(uiIn, uiOut)
-        val toRestore = getItemsToRestore(uiIn, uiOut)
-        MFSelectorResult(toInject, toRestore)
-    }
+    val toInject = getItemsToInject(uiIn, uiOut, replaceAll)
+    val toRestore = getItemsToRestore(uiIn, uiOut)
+    return MFSelectorResult(toInject, toRestore)
 }
 
 private fun RunManagerEx.getConfigurationItems() = allConfigurationsList
@@ -40,6 +34,9 @@ private fun RunConfiguration.templateConfigurationName(): String = type.displayN
 
 private fun RunManagerEx.hasMainframerTask(configuration: RunConfiguration) =
         getBeforeRunTasks(configuration).any { it is MFBeforeRunTask }
+
+private fun getItemsToInject(uiIn: List<MFSelectorItem>, uiOut: List<MFSelectorItem>, replaceAll: Boolean) =
+        getItemsToInject(if (replaceAll) emptyList() else uiIn, uiOut)
 
 private fun getItemsToInject(uiIn: List<MFSelectorItem>, uiOut: List<MFSelectorItem>) =
         (uiOut.filter { it.isSelected } - uiIn.filter { it.isSelected }).map { it.configuration }
