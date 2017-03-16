1d37ad83f41367b80e70076d598e177978e1cde1
Dominik <dominik.malantowicz@gmail.com>
2017-03-16 13:08:06 +0100

    [#126] Replace remote base path with given local path

--- src/test/kotlin/com/elpassion/intelijidea/common/console/RemoteToLocalInputConverterTest.kt
+++ src/test/kotlin/com/elpassion/intelijidea/common/console/RemoteToLocalInputConverterTest.kt
@@ -1,5 +1,6 @@
 package com.elpassion.intelijidea.common.console
 
+import org.assertj.core.api.Assertions
 import org.junit.Assert.assertFalse
 import org.junit.Assert.assertTrue
 import org.junit.Test
@@ -7,6 +8,7 @@ import org.junit.Test
 class RemoteToLocalInputConverterTest {
 
     private val PROJECT_NAME = "testProject"
+    private val localBasePath = "base/path/$PROJECT_NAME"
 
     @Test
     fun `Should catch file path from remote machine`() {
@@ -44,9 +46,15 @@ class RemoteToLocalInputConverterTest {
         assertFalse(converter.FILE_PATH_REGEX.matches("/longer/path/mainframer/$PROJECT_NAME/Example."))
     }
 
+    @Test
+    fun `Should replace remote base path with given local path`() {
+        val converter = RemoteToLocalInputConverter(PROJECT_NAME)
+        val replacedPath = "/longer/path/mainframer/$PROJECT_NAME/Example.kt".replace(converter.FILE_PATH_REGEX, "$localBasePath$1")
+        Assertions.assertThat(replacedPath).isEqualTo("$localBasePath/Example.kt")
+    }
 }
 
 class RemoteToLocalInputConverter(projectName: String) {
     private val FILE_END_PATH_REGEX = "(/.+\\.\\w+)*"
-    val FILE_PATH_REGEX = "((/.+)*/mainframer/$projectName$FILE_END_PATH_REGEX)".toRegex()
+    val FILE_PATH_REGEX = "(?:(?:/.+)*/mainframer/$projectName$FILE_END_PATH_REGEX)".toRegex()
 }
