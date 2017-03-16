1f6664ca902c32de9ebffbb81b7a5462c3aba8ec
Dominik <dominik.malantowicz@gmail.com>
2017-03-16 22:16:00 +0100

    [#126] [refactoring] Remove redundant code

--- src/test/kotlin/com/elpassion/intelijidea/common/console/RemoteToLocalInputConverterTest.kt
+++ src/test/kotlin/com/elpassion/intelijidea/common/console/RemoteToLocalInputConverterTest.kt
@@ -48,27 +48,6 @@ class RemoteToLocalInputConverterTest {
     }
 
     @Test
-    fun `Should catch colon and space signs in first fragment of line`() {
-        assertTrue(converter.FIRST_FRAGMENT_REGEX.matches(": "))
-    }
-
-    @Test
-    fun `Should catch whole first fragment of line`() {
-        assertTrue(converter.FIRST_FRAGMENT_REGEX.matches("Very complicated exception: "))
-    }
-
-    @Test
-    fun `Should check only one line if it matches first fragment regex`() {
-        assertFalse(converter.FIRST_FRAGMENT_REGEX.matches("Very complicated exception\n: "))
-    }
-
-    @Test
-    fun `Should replace first fragment only`() {
-        val replacedPath = "Error: /longer/path/mainframer/$PROJECT_NAME/com/elpassion/mainframer/Example.kt: (19, 10): error".replaceFirst(converter.FIRST_FRAGMENT_REGEX, "<$1>")
-        Assertions.assertThat(replacedPath).isEqualTo("<Error: >/longer/path/mainframer/$PROJECT_NAME/com/elpassion/mainframer/Example.kt: (19, 10): error")
-    }
-
-    @Test
     fun `Should replace remote base path with given local path and have first fragment`() {
         val replacedPath = "Complicated error: /longer/path/mainframer/$PROJECT_NAME/com/elpassion/mainframer/Example.kt: (19, 10): error: Errors everywhere!".replace(converter.LINE_WITH_REMOTE_EXCEPTION, "$localBasePath$1:$2")
         Assertions.assertThat(replacedPath).isEqualTo("Complicated error: $localBasePath/com/elpassion/mainframer/Example.kt:19: error: Errors everywhere!")
@@ -108,16 +87,6 @@ class RemoteToLocalInputConverterTest {
     }
 
     @Test
-    fun `Should catch last fragment of line`() {
-        assertTrue(converter.LAST_FRAGMENT_REGEX.matches(": Rest of line"))
-    }
-
-    @Test
-    fun `Should not catch last fragment of line if it is incorrect`() {
-        assertFalse(converter.LAST_FRAGMENT_REGEX.matches("Rest of line"))
-    }
-
-    @Test
     fun `Should catch path segment`() {
         assertTrue(converter.PATH_SEGMENT.toRegex().matches("/test/test2"))
     }
@@ -131,11 +100,9 @@ class RemoteToLocalInputConverter(projectName: String) {
     private val REMOTE_START_PATH = "(?:$PATH_SEGMENT)*"
     private val REMOTE_PATH = "$REMOTE_START_PATH/mainframer/$projectName"
     val FILE_PATH_REGEX = "(?:$REMOTE_PATH$END_PATH)".toRegex()
-    val FIRST_FRAGMENT_REGEX = "(.*?:\\s)".toRegex()
     private val LINE_NUMBER_START = ":(?:\\s\\()?"
     private val LINE_NUMBER_VALUE = "(\\d+)"
     private val LINE_NUMBER_END = "(?:,\\s\\d+\\))?"
     val LINE_NUMBER_REGEX = "$LINE_NUMBER_START$LINE_NUMBER_VALUE$LINE_NUMBER_END".toRegex()
-    val LAST_FRAGMENT_REGEX = ":\\s.+".toRegex()
     val LINE_WITH_REMOTE_EXCEPTION = "${FILE_PATH_REGEX.pattern}${LINE_NUMBER_REGEX.pattern}".toRegex()
 }
