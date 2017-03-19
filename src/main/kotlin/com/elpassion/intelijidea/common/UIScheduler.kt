603dd168b0c95d050486d0405db561f9cb47be08
Marek Langiewicz <marek.langiewicz@gmail.com>
2017-03-19 23:33:46 +0100

    Simplify custom schedulers - OOP is BAD ;)

--- src/main/kotlin/com/elpassion/intelijidea/common/UIScheduler.kt
+++ src/main/kotlin/com/elpassion/intelijidea/common/UIScheduler.kt
@@ -1,19 +1,8 @@
 package com.elpassion.intelijidea.common
 
 import com.intellij.openapi.application.ApplicationManager
-import io.reactivex.Scheduler
 import io.reactivex.internal.schedulers.ExecutorScheduler
-import java.util.concurrent.Executor
 
-object UIScheduler : Scheduler() {
-
-    override fun createWorker(): Worker {
-        return ExecutorScheduler.ExecutorWorker(UIExecutor)
-    }
-
-    private object UIExecutor : Executor {
-        override fun execute(command: Runnable) {
-            ApplicationManager.getApplication().invokeLater(command)
-        }
-    }
+val UIScheduler = ExecutorScheduler { command ->
+    ApplicationManager.getApplication().invokeLater(command)
 }
--- src/main/kotlin/com/elpassion/intelijidea/task/UINonModalScheduler.kt
+++ src/main/kotlin/com/elpassion/intelijidea/task/UINonModalScheduler.kt
@@ -2,19 +2,8 @@ package com.elpassion.intelijidea.task
 
 import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.application.ModalityState
-import io.reactivex.Scheduler
 import io.reactivex.internal.schedulers.ExecutorScheduler
-import java.util.concurrent.Executor
 
-object UINonModalScheduler : Scheduler() {
-
-    override fun createWorker(): Worker {
-        return ExecutorScheduler.ExecutorWorker(UIExecutor)
-    }
-
-    private object UIExecutor : Executor {
-        override fun execute(command: Runnable) {
-            ApplicationManager.getApplication().invokeLater(command, ModalityState.NON_MODAL)
-        }
-    }
+val UINonModalScheduler = ExecutorScheduler { command ->
+    ApplicationManager.getApplication().invokeLater(command, ModalityState.NON_MODAL)
 }
