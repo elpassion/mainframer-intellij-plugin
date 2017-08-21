04e1c89cf96d5886668fcb18ed223112d5259707
Paweł Gajda <pawegio@gmail.com>
2017-08-21 11:22:49 +0200

    #169 Expect runtime exception on invalid version format

--- src/main/kotlin/com/elpassion/mainframerplugin/util/config.kt
+++ src/main/kotlin/com/elpassion/mainframerplugin/util/config.kt
@@ -5,14 +5,17 @@ import java.util.regex.Pattern
 val toolFilename = "mainframer"
 
 fun getToolDownloadUrl(version: String): String {
-    val filename = toolFilename + if (version.isVersionPre3()) ".sh" else ""
+    val filename = toolFilename + if (version.isPre3) ".sh" else ""
     return "https://github.com/gojuno/mainframer/releases/download/v$version/$filename"
 }
 
-private fun String.isVersionPre3(): Boolean {
-    val matcher = Pattern.compile("^(\\d+)").matcher(this)
-    return if (matcher.find()) {
-        val version = matcher.group(1).toInt()
-        version < 3
-    } else false
-}
+private val String.isPre3: Boolean
+    get() = majorVersion < 3
+
+private val String.majorVersion: Int
+    get() {
+        val matcher = Pattern.compile("^(\\d+)").matcher(this)
+        return if (matcher.find()) {
+            matcher.group(1).toInt()
+        } else throw RuntimeException("Invalid version format!")
+    }
--- src/test/kotlin/com/elpassion/mainframerplugin/util/ToolDownloadTest.kt
+++ src/test/kotlin/com/elpassion/mainframerplugin/util/ToolDownloadTest.kt
@@ -1,5 +1,6 @@
 package com.elpassion.mainframerplugin.util
 
+import com.elpassion.mainframerplugin.common.assertThrows
 import org.hamcrest.core.StringEndsWith.endsWith
 import org.junit.Assert.assertThat
 import org.junit.Test
@@ -14,5 +15,12 @@ class ToolDownloadTest {
     @Test
     fun `Get download url ending with proper filename for pre 3 version`() {
         assertThat(getToolDownloadUrl("2.0.0"), endsWith("$toolFilename.sh"))
+    }
+
+    @Test
+    fun `Expect runtime exception on invalid version format`() {
+        assertThrows<RuntimeException> {
+            getToolDownloadUrl("invalid version")
+        }
     }
 }
